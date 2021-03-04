5f67e8ff7d848a1331b6e83259753979dc8d1a47
yourGitHubusernamebalaji-ramu <balajiramu97@gmail.com>
2021-03-04 14:59:51 +0530

    Client class changes

--- src/test/java/ServicesApiTest.java
+++ src/test/java/ServicesApiTest.java
@@ -33,11 +33,11 @@ public class ServicesApiTest {
         System.out.println("\n The response is "+" "+response[0]);
         statusCode = client.getStatusCode();
 
-        softly.assertThat(response[0].getName()).as("Name").isEqualTo("Kennel Kitche");
+        softly.assertThat(response[0].getName()).as("Name").isEqualTo("Kennel Kitchen");
         softly.assertThat(response[0].getId()).as("ID").isEqualTo(1);
         softly.assertThat(response[0].getAmount()).as("Amount").isEqualTo(635);
         softly.assertThat(response[0].getCurrency()).as("Currency").isEqualTo("$");
-        softly.assertThat(statusCode).isEqualTo(201);
+        softly.assertThat(statusCode).isEqualTo(200);
         softly.assertAll();
 
     }
@@ -53,7 +53,7 @@ public class ServicesApiTest {
 
         softly.assertThat(response1.getAmount()).isEqualTo(451);
         softly.assertThat(response1.getCurrency()).isEqualTo("$");
-        softly.assertThat(response1.getName()).isEqualTo("SD");
+        softly.assertThat(response1.getName()).isEqualTo("Goat");
 
         softly.assertAll();
 
@@ -74,7 +74,7 @@ public class ServicesApiTest {
         statusCode = client1.getStatusCode();
         softly.assertThat(response.getName()).isEqualTo("Goat");
         softly.assertThat(response1.getTotalAmount()).isNotNull();
-        softly.assertThat(statusCode).isEqualTo(201);
+        softly.assertThat(statusCode).isEqualTo(200);
         softly.assertAll();
 
         System.out.println("The total amount in order summary is"+" "+response1.getTotalAmount());
@@ -87,7 +87,7 @@ public class ServicesApiTest {
         ServicesApiClient client = new ServicesApiClient(apiUrl,"/api/shop/clearcart/");
         ApiResponse<Response[]> response = client.clearCart();
         statusCode = client.getStatusCode();
-        softly.assertThat(statusCode).isEqualTo(201);
+        softly.assertThat(statusCode).isEqualTo(400);
 
         ServicesApiClient client1 = new ServicesApiClient(apiUrl, "/api/shop/ordersummary");
         Response response1 = client1.getOrderSummary().getContent();
